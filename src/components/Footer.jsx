c9b5b58e4f9e97ade1b4feff2d85c801f2bdad14
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-12 16:14:59 -0600

    starting with react components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    const date = new Date()
+    const year = date.getFullYear()
+
+    return ( <footer>
+    <p>Copyright © {year} 
+    </p>
+    </footer>)
+}
+
+export default Footer
